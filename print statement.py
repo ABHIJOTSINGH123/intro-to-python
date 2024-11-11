a13b0d9a7da7ccd5dc6641aa4faa03cb1ee50576
Abhijot singh <manjeetkaur2w00786@gmail.com>
2024-11-11 07:06:47 -0800

    python

---
+++ print statement.py
@@ -0,0 +1,7 @@
+print("hello world")
+#printing my name.
+print("my name is ABHIJOT SINGH")
+#printing a fun fact about myself.
+print("my hobby is to do gatka")
+# performing and printing simple calculations.
+print("the result of 3+5 is",3+5)
